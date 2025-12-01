df43199415e27d83abe278f78dac355c0e373378
bakyybroklim-hash <bakyybroklim@gmail.com>
2025-12-01 20:50:18 +0600

    Add files via upload

---
+++ 12dom/skript.js
@@ -0,0 +1,11 @@
+let resumeData = {
+name: "bakyy",
+age: 22,
+homeland: "issyk-kul",
+education: "university",
+profession: "developer",
+skills: ["html", "css",],
+languages: ["russian", "kyrgyz",], 
+hobbys: ["football", "programming","atheletics"],
+}
+console.log(resumeData);
